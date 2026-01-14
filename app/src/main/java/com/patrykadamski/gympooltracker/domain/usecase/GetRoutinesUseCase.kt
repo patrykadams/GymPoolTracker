eddd48018112ef5bef622f482dc63779998e6455
Patryk Adamski <patrykadamski123@gmail.com>
2026-01-14 17:30:29 +0100

    fix: Update GetRoutinesUseCase to use correct repository method
    - Changed `repository.getRoutines()` to `repository.getAllRoutines()`
    - Resolved "Unresolved reference" error matching the RoutineRepository interface

--- app/src/main/java/com/patrykadamski/gympooltracker/domain/usecase/GetRoutinesUseCase.kt
+++ app/src/main/java/com/patrykadamski/gympooltracker/domain/usecase/GetRoutinesUseCase.kt
@@ -1,7 +1,7 @@
 // file: app/src/main/java/com/patrykadamski/gympooltracker/domain/usecase/GetRoutinesUseCase.kt
 package com.patrykadamski.gympooltracker.domain.usecase
 
-import com.patrykadamski.gympooltracker.domain.model.Routine
+import com.patrykadamski.gympooltracker.data.local.RoutineWithExercises
 import com.patrykadamski.gympooltracker.domain.repository.RoutineRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,7 +9,7 @@ import javax.inject.Inject
 class GetRoutinesUseCase @Inject constructor(
     private val repository: RoutineRepository
 ) {
-    operator fun invoke(): Flow<List<Routine>> {
-        return repository.getRoutines()
+    operator fun invoke(): Flow<List<RoutineWithExercises>> {
+        return repository.getAllRoutines()
     }
 }
